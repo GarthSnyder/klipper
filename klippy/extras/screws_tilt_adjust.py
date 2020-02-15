76ad9347c8f4d17a56a02cc7a179a1d4973e515b
Garth Snyder <garth@garthsnyder.com>
2020-02-15 12:05:23 -0800

    Test: use offsets for SCREWS_TILT_ADJUST

--- klippy/extras/screws_tilt_adjust.py
+++ klippy/extras/screws_tilt_adjust.py
@@ -41,6 +41,7 @@ class ScrewsTiltAdjust:
                                                     self.probe_finalize,
                                                     default_points=points)
         self.probe_helper.minimum_points(3)
+        self.probe_helper.use_offsets(True)
         # Register command
         self.gcode = self.printer.lookup_object('gcode')
         self.gcode.register_command("SCREWS_TILT_CALCULATE",
